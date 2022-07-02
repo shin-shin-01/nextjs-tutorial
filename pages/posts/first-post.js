c0dc7fe4458078021809efcb631855fef2d3f3fd
Kaze Shindo <kaze.bknb@outlook.jp>
2022-07-02 12:07:54 +0900

    feat: Link navigation

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+
 export default function FirstPost() {
-  return <h1>First Post</h1>;
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+ 
+        {/* // Server-Side Navigation */}
+        {/* <a href="/">Back to home</a> */}
+      </h2>
+    </>
+  );
 }
