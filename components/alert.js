15b759cd0d2866669b6b084a17f2e4c3f5327aec
Kaze Shindo <kaze.bknb@outlook.jp>
2022-07-02 14:07:16 +0900

    feat: AlertPage (using classnames library to toggle classes)

---
+++ components/alert.js
@@ -0,0 +1,17 @@
+import styles from './alert.module.css'
+import cn from 'classnames';
+
+export default function Alert({ children, type }) {
+  return (
+    <div
+      className={ `${styles.center} ${cn({
+        [styles.success]: type === 'success',
+        [styles.warning]: type === 'warning',
+        [styles.error]: type === 'error',
+      })}
+      `}
+    >
+      {children}
+    </div>
+  );
+}
---
+++ pages/alert/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+import Alert from '../../components/alert';
+
+export default function AlertPage() {
+  return (
+    <Layout>
+      <Head>
+        <title>Alert Test</title>
+      </Head>
+      <div>
+        <Alert type='success'>
+          <p>AlertTest: SUCCESS</p>
+        </Alert>
+        <Alert type='warning'>
+          <p>AlertTest: WARNING</p>
+        </Alert>
+        <Alert type='error'>
+          <p>AlertTest: ERROR</p>
+        </Alert>
+      </div>
+    </Layout>
+  );
+}
