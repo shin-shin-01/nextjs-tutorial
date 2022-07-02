edbeb5bb58d9f255346d581e99e9a3c0d97256ed
Kaze Shindo <kaze.bknb@outlook.jp>
2022-07-02 21:06:19 +0900

    refactor: convert js to ts

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,7 +1,13 @@
 import styles from './alert.module.css'
-import cn from 'classnames';
+import cn from 'classnames'
 
-export default function Alert({ children, type }) {
+export default function Alert({
+  children,
+  type
+}: {
+  children: React.ReactNode
+  type: string
+}) {
   return (
     <div
       className={ `${styles.center} ${cn({
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Date from '../components/date';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
+import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -46,4 +46,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
